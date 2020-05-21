064a8b7a7692b12d119448b31c3f93acad9cdbbe
nkkf <nelsonkenmochi@gmail.com>
2020-05-20 21:39:06 -0400

    Users list added

--- backend/index.js
+++ backend/index.js
@@ -4,15 +4,25 @@ const io = require('socket.io')(http);
 
 const PORT = process.env.PORT || 3000;
 
+const usersList = [];
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
 io.on('connection', (socket) => {
     console.log(`User Connected -> ${socket.id}`);
+    usersList.push(socket.id);
+    io.emit('users update', usersList);
 
     socket.on('disconnect', () => {
         console.log(`User Disconnected -> ${socket.id}`);
+        usersList.splice(usersList.indexOf(socket.id), 1);
+
+        console.log(usersList);
+        console.log(' - - - ');
+
+        io.emit('users update', usersList);
     });
 
     socket.on('chat message', (speaker, message) => {
